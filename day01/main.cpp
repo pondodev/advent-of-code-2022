774d876a63c84c2934a02ab5bb1f132e452d7e5d
Daniel Coady <me@pondo.dev>
2022-12-02 12:15:44 +1100

    day 1 part 2

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 
 #include "helpers.hpp"
 
 int main() {
 	const auto input = Helpers::ReadInput();
+
+	// part 1
+	{
 
 	uint32_t max = 0;
 	uint32_t current = 0;
@@ -13,12 +17,40 @@ int main() {
 			if ( current > max ) max = current;
 			current = 0;
 		} else { // add to current inventory
-			auto calories = std::stoi(item);
+			auto calories = std::stoi( item );
 			current += calories;
 		}
 	}
 
 	std::cout << "max is: " << max << std::endl;
+
+	}
+
+	// part 2
+	{
+
+	uint32_t current = 0;
+	std::vector<uint32_t> inventories;
+
+	for ( const auto& item : input ) {
+		if ( item == "" ) { // end of current inventory
+			inventories.push_back( current );
+			current = 0;
+		} else { // add to current inventory
+			auto calories = std::stoi( item );
+			current += calories;
+		}
+	}
+
+	std::sort( inventories.begin(), inventories.end(), [](const uint32_t& a, const uint32_t& b) -> bool { return a > b; } );
+
+	uint32_t sum = 0;
+	constexpr uint32_t kTopCount = 3;
+	for ( size_t i = 0; i < kTopCount; ++i ) sum += inventories[ i ];
+
+	std::cout << "sum is: " << sum << std::endl;
+
+	}
 
 	return 0;
 }
